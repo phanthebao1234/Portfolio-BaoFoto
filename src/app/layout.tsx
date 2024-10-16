91c71fc63db8fd6f0f7f08d621ef78a7d758ec46
phanthebao1234 <phanthebao888@gmail.com>
2024-10-16 09:58:04 +0700

    setting three.js scence & add sound effects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Urbanist } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const urbanist = Urbanist({ subsets: ['latin']})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <html lang="en" className="bg-slate-900 text-slate-100">
+      <body 
+        className={urbanist.className}
       >
         {children}
       </body>
