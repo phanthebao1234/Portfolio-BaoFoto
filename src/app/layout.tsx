b996789c290054b590b65dd6cb69e1eb229eae84
phanthebao1234 <phanthebao888@gmail.com>
2024-10-29 22:46:48 +0700

    Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Urbanist } from "next/font/google";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const urbanist = Urbanist({ subsets: ['latin']})
 
@@ -22,7 +23,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
-        <div className="h-[60vh]"></div>
+        <Footer />
       </body>
     </html>
   );
