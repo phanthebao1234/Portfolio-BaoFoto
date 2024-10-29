fe904d2412af59b4e06533ac0ace4962f314b7a7
phanthebao1234 <phanthebao888@gmail.com>
2024-10-29 22:39:08 +0700

    Navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,12 @@
 import { createClient } from "@/prismicio";
-import { PrismicNextLink } from "@prismicio/next";
-import Link from "next/link";
+import Navbar  from '@/components/Navbar'
 
 export default async function Header() {
     const client = createClient();
     const settings = await client.getSingle("settings");
     return (
-        <header className="top-0 z-50 mx-auto max-w-7x1 md:sticky md:top-4">
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/" aria-label="Home Page">{settings.data.name}</Link>
-                    </li>
-                    {settings.data.nav_item.map(({link, label}, index) => (
-                        <li key={index}>
-                            <PrismicNextLink field={link}>{label}</PrismicNextLink>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
+        <header className="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4">
+            <Navbar settings={settings} />
         </header>
     )
 };
